bf9dd3a77db76a30b47127f54c28b264670ef9e4
Banderan <bartoszwojcik2004@gmail.com>
2026-06-28 08:46:15 +0200

    init be project

---
+++ src/main/java/eu/relay4u/prospecting/EuRelay4uProspectingBeApplication.java
@@ -0,0 +1,13 @@
+package eu.relay4u.prospecting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuRelay4uProspectingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuRelay4uProspectingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/relay4u/prospecting/EuRelay4uProspectingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.relay4u.prospecting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuRelay4uProspectingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
