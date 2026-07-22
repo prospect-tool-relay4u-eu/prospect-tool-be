a9c1af486a038545d14ded93a2116ad03b283e83
Karol Gajda <karolgaj97@interia.pl>
2026-07-22 15:32:05 +0200

    minor changing: delete sandbox dll-auto, adding max length to FieldType

--- src/main/java/eu/relay4u/prospecting/model/ProjectField.java
+++ src/main/java/eu/relay4u/prospecting/model/ProjectField.java
@@ -32,7 +32,7 @@ public class ProjectField {
     private String label;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 20)
     private FieldType type;
 
     private boolean required = false;
