af3e0e275aadf4df76e10157a5d8f89ad7159165
michalgalus <galusmichal@outlook.com>
2026-08-02 22:13:23 +0200

    dev-jv-in-app-notifications added NotificationDto

---
+++ src/main/java/eu/relay4u/prospecting/dto/notification/NotificationDto.java
@@ -0,0 +1,15 @@
+package eu.relay4u.prospecting.dto.notification;
+
+import eu.relay4u.prospecting.model.User;
+
+import java.time.LocalDateTime;
+
+public record NotificationDto(
+        Long notificationId,
+        String type,
+        String title,
+        String message,
+        Boolean isRead,
+        String resourceLink,
+        LocalDateTime createdAt) {
+}
