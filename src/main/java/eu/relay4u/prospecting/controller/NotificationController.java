a506d58c686aeb58608cdb4145abb923dfecc9f7
michalgalus <galusmichal@outlook.com>
2026-08-05 17:34:32 +0200

    dev-jv-in-app-notifications added NotificationController

---
+++ src/main/java/eu/relay4u/prospecting/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package eu.relay4u.prospecting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class NotificationController {
+}
