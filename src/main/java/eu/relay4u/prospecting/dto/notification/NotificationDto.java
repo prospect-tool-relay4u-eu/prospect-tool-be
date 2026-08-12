133d0df75b9d498a3417000cc2e181a973314c8a
michalgalus <galusmichal@outlook.com>
2026-08-12 06:48:32 +0200

    dev-jv-in-app-notifications added NotificationType to dto

--- src/main/java/eu/relay4u/prospecting/dto/notification/NotificationDto.java
+++ src/main/java/eu/relay4u/prospecting/dto/notification/NotificationDto.java
@@ -1,12 +1,13 @@
 package eu.relay4u.prospecting.dto.notification;
 
+import eu.relay4u.prospecting.model.NotificationType;
 import eu.relay4u.prospecting.model.User;
 
 import java.time.LocalDateTime;
 
 public record NotificationDto(
         Long notificationId,
-        String type,
+        NotificationType type,
         String title,
         String message,
         Boolean isRead,
--- src/main/java/eu/relay4u/prospecting/dto/notification/NotificationRequest.java
+++ src/main/java/eu/relay4u/prospecting/dto/notification/NotificationRequest.java
@@ -1,9 +1,11 @@
 package eu.relay4u.prospecting.dto.notification;
 
+import eu.relay4u.prospecting.model.NotificationType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record NotificationRequest(
-        @NotBlank String type,
+        @NotNull NotificationType type,
         @NotBlank String title,
         @NotBlank String message,
         String resourceLink) {
